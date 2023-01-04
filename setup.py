6eba0c85da3102d8d0e19d123feff59d57ea036b
anthonywong611 <anthonywong2718@gmail.com>
2023-01-03 20:52:31 -0500

    created folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyze borrowers' employment and credit history to predict borrower'',
+    author='Anthony Wong',
+    license='',
+)
