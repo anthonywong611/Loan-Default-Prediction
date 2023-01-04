dd6c980ad2c9ee91964cb6fd4dd3e798ef62b987
anthonywong611 <anthonywong2718@gmail.com>
2023-01-03 21:12:21 -0500

    changed setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='Analyze borrowers' employment and credit history to predict borrower'',
-    author='Anthony Wong',
-    license='',
+   name='src',
+   packages=find_packages(),
+   version='0.1.0',
+   description="Analyze past borrowers' employment and credit history to predict new borrower's behavior in defaulting a loan.",
+   author='Anthony Wong',
+   license='',
 )
